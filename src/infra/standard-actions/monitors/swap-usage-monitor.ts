24b8eecf73fa2528577c4782ff0c8450873e6ed5
Wesley0010012 <wglaurindo33@gmail.com>
2025-07-26 17:56:12 -0300

    feat: add swap usage monitor

---
+++ src/infra/standard-actions/monitors/swap-usage-monitor.ts
@@ -0,0 +1,25 @@
+import { Usage } from '@/domain/entities/usage'
+import { InternalError } from 'project-custom-errors'
+import { NotifiableStandardAction } from '@/application/notifiable-standard-action'
+import { StandardAction } from '@/domain/protocols/standard-action'
+import si from 'systeminformation'
+
+export class SwapUsageMonitor extends NotifiableStandardAction {
+  public constructor(messageNotifier: StandardAction<string, void>) {
+    super(
+      'Swap usage',
+      messageNotifier,
+      Number.parseInt(process.env.SWAP_PERCENTAGEM_LIMIT || '80'),
+      Number.parseInt(process.env.SWAP_PERCENTAGE_NOTIFICATION_INTERVAL || '1')
+    )
+  }
+
+  protected async getUsage(): Promise<Usage> {
+    try {
+      const mem = await si.mem()
+      return new Usage(mem.swaptotal, mem.swapused)
+    } catch {
+      throw new InternalError('Cannot access swap usage information')
+    }
+  }
+}

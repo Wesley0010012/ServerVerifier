4cd98ddbd1c4ff00e62a07a3784e71c4d18a67f0
Wesley0010012 <wglaurindo33@gmail.com>
2025-07-26 17:56:25 -0300

    feat: add system load monitor

---
+++ src/infra/standard-actions/monitors/system-load-monitor.ts
@@ -0,0 +1,25 @@
+import { Usage } from '@/domain/entities/usage'
+import { InternalError } from 'project-custom-errors'
+import { NotifiableStandardAction } from '@/application/notifiable-standard-action'
+import { StandardAction } from '@/domain/protocols/standard-action'
+import { loadavg } from 'os'
+
+export class SystemLoadMonitor extends NotifiableStandardAction {
+  public constructor(messageNotifier: StandardAction<string, void>) {
+    super(
+      'System Load',
+      messageNotifier,
+      Number.parseInt(process.env.LOAD_PERCENTAGEM_LIMIT || '80'),
+      Number.parseInt(process.env.LOAD_PERCENTAGE_NOTIFICATION_INTERVAL || '1')
+    )
+  }
+
+  protected async getUsage(): Promise<Usage> {
+    try {
+      const load = loadavg()[0]
+      return new Usage(100, load)
+    } catch {
+      throw new InternalError('Cannot access uptime/load information')
+    }
+  }
+}

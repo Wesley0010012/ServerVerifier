f23104fd6fb05642798d24c6bee1c3604d225d60
Wesley0010012 <wglaurindo33@gmail.com>
2025-08-03 19:14:50 -0300

    refactor: implementa protocolo Runner

--- src/application/controllers/server-verifier-controller.ts
+++ src/application/controllers/server-verifier-controller.ts
@@ -1,12 +1,16 @@
 import { StandardAction } from '@/domain/protocols/standard-action'
+import { Runner } from '../protocols/runner'
 
-export class ServerVerifierController {
+export class ServerVerifierController implements Runner {
+  private intervalId: NodeJS.Timeout
+
   public constructor(
     private readonly actions: StandardAction<void, void>[],
-    private readonly messageNotifier: StandardAction<string, void>
+    private readonly messageNotifier: StandardAction<string, void>,
+    private readonly intervalMs: number
   ) {}
 
-  public async execute() {
+  private async execute() {
     try {
       const activities = this.actions.map(action => action.process())
 
@@ -15,6 +19,18 @@ export class ServerVerifierController {
       await this.messageNotifier.process(
         `Error occur in system during execution: ${(e as Error).message}`
       )
+    }
+  }
+
+  public async start(): Promise<void> {
+    this.intervalId = setInterval(async () => {
+      await this.execute()
+    }, this.intervalMs)
+  }
+
+  public async stop(): Promise<void> {
+    if (this.intervalId) {
+      clearInterval(this.intervalId)
     }
   }
 }
